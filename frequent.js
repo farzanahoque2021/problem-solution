cc374175be83ec0d76c1dd3700241d8263ec20c9
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-07-25 20:58:50 +0600

    solution of problem 3

---
+++ frequent.js
@@ -0,0 +1,24 @@
+function mostFrequent(arr) {
+    arr.sort();
+    let maxCount = 1, result = arr[0];
+    let currCount = 1;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] == arr[i - 1]) {
+            currCount++;
+        }
+        else {
+            currCount = 1;
+        }
+        if (currCount > maxCount) {
+            maxCount = currCount;
+            result = arr[i - 1];
+        }
+
+    }
+
+    return result;
+}
+
+let arr = [3, 5, 2, 5, 3, 3, 1, 4, 5];
+console.log(mostFrequent(arr))
