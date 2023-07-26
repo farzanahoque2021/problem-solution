c24503ebab0078f4cfbc42ef35f66c76b75049a0
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-07-26 20:21:07 +0600

    solution of task 7

---
+++ roman.js
@@ -0,0 +1,16 @@
+function romanToInteger(str) {
+    const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let res = 0;
+    str.split('').forEach((num, i) => {
+        if (map[num] < map[str[i + 1]]) {
+            res -= map[num]
+        }
+        else {
+            res += map[num]
+        }
+    })
+    return res;
+}
+
+let result = romanToInteger("III")
+console.log(result)
