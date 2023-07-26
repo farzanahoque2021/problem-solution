d385414d8e4ef17b97af0cd2bba25970612a83ca
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-07-26 20:39:08 +0600

    solution of task 6

---
+++ generate.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let passwordLength = 8;
+    let password = "";
+    for (let i = 0; i <= passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1)
+    }
+    return console.log(password)
+}
+
+// generatePassword();
